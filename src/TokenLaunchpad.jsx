7ddfc54b3c335528d242a15a84ad9e89ede48905
yagyanssh <yagyansh9999@gmail.com>
2024-09-21 01:41:13 +0530

    init

---
+++ src/TokenLaunchpad.jsx
@@ -0,0 +1,32 @@
+export function TokenLaunchpad() {
+
+    function createToken () {
+
+        const name = document.getElementById("name").value;
+        const symbol = document.getElementById("symbol").value;
+        const imageUrl = document.getElementById("image").value;
+        const initialSupply = document.getElementById("supply").value;
+
+        console.log({
+            name,
+            symbol,
+            imageUrl,
+            initialSupply
+        })
+    }
+
+    return <div style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+    }}>
+        <h1>Solana Token Launchpad</h1>
+        <input id="name" className="inputText" type="text" placeholder="Name" /> <br />
+        <input id="symbol" className="inputText" type="text" placeholder="Symbot" /> <br />
+        <input id="image" className="inputText" type="text" placeholder="ImageURL" /> <br />
+        <input id="supply" className="inputText" type="text" placeholder="Initial Supply" /> <br />
+        <button onClick={createToken} className="btn">Create a Token</button>
+    </div>
+}
